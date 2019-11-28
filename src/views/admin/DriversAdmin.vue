9414f7e78edc198ecd12025b8c8d7b5019827989
teraln <alen.terzic.fs@gmail.com>
2019-11-28 19:34:24 +0100

    Changed design of DriversAdmin view

--- src/views/admin/DriversAdmin.vue
+++ src/views/admin/DriversAdmin.vue
@@ -2,30 +2,51 @@
   <div class="DriversAdmin">
     <v-container>
       <v-row justify="center">
-        <v-col cols="7">
-          <v-row class="mb-5" justify="space-between">
+        <v-col cols="10" md="12">
+          <v-row no-gutters class="mb-5" justify="space-between">
             <H1>Drivers</H1>
 
             <v-spacer></v-spacer>
 
             <DriversModalNew :getDrivers="getDrivers" />
           </v-row>
+          <v-row>
+            <v-col
+              v-for="driver in allDrivers"
+              :key="allDrivers.indexOf(driver)"
+              cols="12"
+              md="4"
+              sm="6"
+              xs="12"
+            >
+              <v-card class="mx-auto" max-width="344" min-width="260" outlined>
+                <v-list-item three-line>
+                  <v-list-item-content>
+                    <div class="overline mb-4">{{ driver.role }}</div>
+                    <v-list-item-title
+                      class="headline mb-1"
+                    >{{ `${driver.name} ${driver.lastname}` }}</v-list-item-title>
+                    <v-list-item-subtitle>{{ driver.series }}</v-list-item-subtitle>
+                  </v-list-item-content>
 
-          <v-banner
-            class="text-capitalize my-1"
-            v-for="driver in allDrivers"
-            :key="allDrivers.indexOf(driver)"
-            single-line
-          >
-            <v-avatar class="mr-5" size="100px">
-              <img :src="driver.imageURL" alt="No image" />
-            </v-avatar>
-            {{ `${driver.name} ${driver.lastname}` }}
-            <template v-slot:actions>
-              <DriversModalEdit :driver="driver" :getDrivers="getDrivers" />
-              <v-btn text outlined color="accent" @click="deleteDriver(driver.id)">delete</v-btn>
-            </template>
-          </v-banner>
+                  <v-list-item-avatar tile size="80" color="grey">
+                    <img :src="driver.imageURL" alt="Avatar" />
+                  </v-list-item-avatar>
+                </v-list-item>
+
+                <v-card-actions>
+                  <DriversModalEdit :driver="driver" :getDrivers="getDrivers" />
+                  <v-btn
+                    text
+                    outlined
+                    class="mx-1"
+                    color="accent"
+                    @click="deleteDriver(driver.id)"
+                  >delete</v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-col>
+          </v-row>
         </v-col>
       </v-row>
     </v-container>
