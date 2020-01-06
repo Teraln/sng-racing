2c4fbcab8eac7aa722889f5a93fe1974566a4e5a
teraln <alen.terzic.fs@gmail.com>
2020-01-06 23:00:26 +0100

    Partners MWP finished

--- src/components/Elements/PartnerCard.vue
+++ src/components/Elements/PartnerCard.vue
@@ -1,16 +1,16 @@
 <template>
   <v-container class="DriverCard">
-    <v-row justify="center">
-      <v-col cols="12">
-        <v-img :src="partner.imageURL"></v-img>
-      </v-col>
-      <v-col cols="12">
-        <p>{{ partner.text }}</p>
-      </v-col>
+    <v-col cols="4" offset="4">
+      <v-img :src="partner.imageURL"></v-img>
+    </v-col>
+    <v-col cols="8" offset="2" class="pa-5 mt-5" id="text-div">
+      <p min-height="200">{{ partner.text }}</p>
+    </v-col>
+    <v-col cols="4" offset="4" id="link-container" class="pb-5">
       <a :href="partner.link">
         <v-btn id="website-link" outlined>Website</v-btn>
       </a>
-    </v-row>
+    </v-col>
   </v-container>
 </template>
 
@@ -23,3 +23,26 @@ export default {
   }
 };
 </script>
+
+<style lang="scss" scoped>
+#text-div {
+  border-top: solid 1px $primary;
+  font-size: $fSmall;
+  height: 200px;
+}
+
+#link-container {
+    display: flex;
+    justify-content: center;
+  border-bottom: solid 1px $primary;
+
+  #website-link {
+    transition: background-color 0.5s;
+    transition: color 0.5s;
+    &:hover {
+      background-color: $primary;
+      color: $secondary;
+    }
+  }
+}
+</style>
