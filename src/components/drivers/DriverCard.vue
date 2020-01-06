ba3a5aa16487334ca955ef834a2c3fa3b2e69ad4
teraln <alen.terzic.fs@gmail.com>
2020-01-06 12:09:54 +0100

    Basic layout of drivercard done

--- src/components/drivers/DriverCard.vue
+++ src/components/drivers/DriverCard.vue
@@ -1,43 +1,73 @@
 <template>
   <v-container class="DriverCard">
     <v-row>
-      <v-col cols="6">
-        <v-img
-          src="https://bilder.t-online.de/b/85/67/15/44/id_85671544/610/tid_da/ayrton-senna-im-overall-seines-damaligen-teams-williams-renault-.jpg"
-        ></v-img>
+      <v-col cols="12" md="5">
+        <v-img :src="driver.imageURL" max-height="300"></v-img>
       </v-col>
-      <v-col cols="6">
-        <v-list-item>
-          <v-list-item-content>
-            <li id="driver-info">
-              <span class="mr-4" id="property">Country:</span>
-              <span id="info">Netherlands</span>
-            </li>
-          </v-list-item-content>
-        </v-list-item>
+      <v-col cols="12" md="7">
+        <v-col class="py-2 px-4" cols="12">
+          <v-row>
+            <v-col id="property" class="py-0" cols="3">Country:</v-col>
+            <v-col id="info" class="py-0" cols="9">{{ driver.country }}</v-col>
+          </v-row>
+        </v-col>
+        <v-col class="py-2 px-4" cols="12">
+          <v-row>
+            <v-col id="property" class="py-0" cols="3">Age:</v-col>
+            <!-- TODO temp -->
+            <v-col id="info" class="py-0" cols="9">{{ driver.day }}</v-col>
+          </v-row>
+        </v-col>
+        <v-col class="py-2 px-4" cols="12">
+          <v-row>
+            <v-col id="property" class="py-0" cols="3">Role:</v-col>
+            <v-col id="info" class="py-0" cols="9">{{ driver.role }}</v-col>
+          </v-row>
+        </v-col>
+        <v-col class="py-2 px-4" cols="12">
+          <v-row>
+            <v-col id="property" class="py-0" cols="3">Country:</v-col>
+            <v-col id="info" class="py-0" cols="9">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores laborum aut quisquam quae magnam, sequi nobis doloribus obcaecati alias iusto provident consectetur amet voluptates nulla. Aut commodi saepe alias cupiditate!</v-col>
+          </v-row>
+        </v-col>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
+import API from "../../store/API";
+
 export default {
   name: "DriverCard",
+  props: ["driver"],
   data() {
     return {};
+  },
+  methods: {
+    getAge(e) {
+      //Birthdate
+      const unixBirth = API.dateToUnix(e.year, e.month, e.day);
+      console.log(unixBirth);
+
+      //Today
+      const today = new Date();
+      const unixToday = Date.parse(today);
+      console.log(unixToday);
+
+      //Age
+      // const age = unixToday - unixBirth
+    }
   }
 };
 </script>
 
 <style lang="scss" scoped>
-#driver-info {
-  color: $accent;
+#property {
   font-size: $fText;
-  & #property {
-    font-weight: bold;
-  }
-  & #info {
-    font-weight: normal;
-  }
+  text-align: end;
+}
+#info {
+  font-size: $fText;
 }
 </style>
