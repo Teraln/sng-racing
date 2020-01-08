f179da7cf13a41261c237e823dde6dda76765d48
teraln <alen.terzic.fs@gmail.com>
2020-01-08 17:48:33 +0100

    Uninstalled vue-awesome-swiper

--- src/components/PartnerBelt.vue
+++ src/components/PartnerBelt.vue
@@ -1,57 +1,27 @@
 <template>
   <div id="PartnerBelt">
     <v-container>
-      <!--Swiper-->
       <v-card>
-        <swiper :options="swiperOption">
-          <swiper-slide v-for="i in tempArr" :key="i">
-            <v-card>
-              <v-img
-                class="white--text align-end"
-                height="120px"
-                :src="`https://i.picsum.photos/id/23${i}/300/300.jpg`"
-              ></v-img>
-            </v-card>
-          </swiper-slide>
-        </swiper>
+
       </v-card>
     </v-container>
   </div>
 </template>
 
 <script>
-import "swiper/dist/css/swiper.css";
-import { swiper, swiperSlide } from "vue-awesome-swiper";
+
 
 export default {
   name: "PartnerBelt",
   components: {
-    swiper,
-    swiperSlide
+
   },
   data() {
-    return {
-      //TODO delet
-      //TODO will have to make a script that adds elements to the array to get rid of blanks?
-      tempArr: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-      swiperOption: {
-        slidesPerView: 5,
-        slidesPerGroup: 1,
-        loopedSlides: 4,
-        spaceBetween: 30,
-        loop: true,
-        loopFillGroupWithBlank: false,
-        autoplay: {
-          delay: 1000
-        }
-      }
-    };
+    return { };
   }
 };
 </script>
 
 <style lang="scss" scoped>
-.swiper-container {
-  background-color: $secondary;
-}
+
 </style>
