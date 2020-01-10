49e897472a4eabe33e155be2704cf8f0fbf26167
teraln <alen.terzic.fs@gmail.com>
2020-01-10 15:46:00 +0100

    Gallery styling in progress

--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -1,14 +1,29 @@
 <template>
   <div class="Gallery">
     <Navbar />
+    <v-btn @click="smoothScroll" text>button</v-btn>
     <TopParallax />
     <PartnerBelt />
-    <v-container>
-      <hooper id="gallery-hooper" :settings="hooperSettings">
-        <slide v-for="(item, index) in gallery" :key="index" :index="index">
-          <v-img :src="item.imageURL" :alt="item.title"></v-img>
-          <h1>{{item.title}}</h1>
+    <v-container id="gallery-container">
+      <!-- <h1 id="gallery-title">Gallery</h1> -->
+      <!-- Control Buttons -->
+      <v-btn id="control-btn-next" @click="slideNext" text x-large icon color="primary">
+        <v-icon>mdi-chevron-down</v-icon>
+      </v-btn>
+      <v-btn id="control-btn-prev" @click="slidePrev" text x-large icon color="primary">
+        <v-icon>mdi-chevron-up</v-icon>
+      </v-btn>
+      <!-- Hooper -->
+      <hooper id="gallery-hooper" :settings="hooperSettings" ref="carousel">
+        <!-- Slides -->
+        <slide id="gallery-slide" v-for="(item, index) in gallery" :key="index" :index="index">
+          <v-img contain id="item-image" :src="item.imageURL" :alt="item.title">
+            <div id="item-title-container">
+              <h5 id="item-title">{{item.title}}</h5>
+            </div>
+          </v-img>
         </slide>
+        <hooper-progress slot="hooper-addons"></hooper-progress>
         <hooper-pagination slot="hooper-addons"></hooper-pagination>
       </hooper>
     </v-container>
@@ -21,7 +36,12 @@ import Navbar from "../components/Navbar";
 import TopParallax from "../components/TopParallax";
 import PartnerBelt from "../components/PartnerBelt";
 
-import { Hooper, Slide, Pagination as HooperPagination } from "hooper";
+import {
+  Hooper,
+  Slide,
+  Pagination as HooperPagination,
+  Progress as HooperProgress
+} from "hooper";
 
 import "hooper/dist/hooper.css";
 
@@ -33,20 +53,40 @@ export default {
     PartnerBelt,
     Hooper,
     Slide,
-    HooperPagination
+    HooperPagination,
+    HooperProgress
   },
   data() {
     return {
       gallery: [],
       hooperSettings: {
         centerMode: true,
-        itemsToShow: 1.2,
         vertical: true,
+        itemsToShow: 1,
         infiniteScroll: true,
-        wheelControl: true,
-        transition: 400
+        transition: 600,
+        wheelControl: false,
+        mouseDrag: false,
+        touchDrag: false,
+        keysControl: true
       }
     };
+  },
+  methods: {
+    slideNext() {
+      this.$refs.carousel.slideNext();
+    },
+    slidePrev() {
+      this.$refs.carousel.slidePrev();
+    },
+    //TODO Maybe something for later
+    smoothScroll() {
+      const galleryView = document.querySelector("#gallery-hooper");
+
+      galleryView.scrollIntoView({
+        behavior: "smooth"
+      });
+    }
   },
   mounted() {
     this.gallery = this.$store.getters.gallery;
@@ -54,7 +94,7 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 .Gallery {
   background: rgb(35, 35, 35);
   background: linear-gradient(
@@ -63,8 +103,59 @@ export default {
     rgba(59, 58, 58, 1) 50%,
     rgba(35, 35, 35, 1) 95%
   );
-  & #gallery-hooper {
-    height: 50vh;
+  //Component wrapper
+  & #gallery-container {
+    //Title
+    & #gallery-title {
+      text-align: center;
+      margin: 3rem 0 4rem 0;
+      font-weight: 300;
+    }
+    //The hooper component
+    & #gallery-hooper {
+      background-color: rgba($color: $grey, $alpha: 1);
+      height: 80vh;
+      margin: 10% 0 10% 0;
+      display: flex;
+      flex-direction: row;
+      //Nav buttons
+      & & #gallery-slide {
+        //Image element
+        & #item-image {
+          overflow: hidden;
+          position: relative;
+          width: 100%;
+          height: auto;
+          //Item title container
+          & #item-title-container {
+            transform: skew(15deg, 0deg);
+            position: absolute;
+            max-width: 70%;
+            top: 5%;
+            left: 5%;
+            background: rgba($color: $lightGrey, $alpha: 0.7);
+            //Item title
+            & #item-title {
+              transform: skew(-15deg, -0deg);
+              padding: 0.1rem 1rem 0.1rem 1rem;
+              color: $accent;
+              font-size: $fText;
+              font-weight: 300;
+            }
+          }
+        }
+      }
+      & .hooper-progress-inner {
+        background-color: $primary;
+      }
+      & .hooper-slide {
+        display: inherit;
+        align-items: center;
+      }
+      & .hooper-indicator.is-active {
+        background-color: $primary;
+      }
+    }
   }
 }
 </style>
