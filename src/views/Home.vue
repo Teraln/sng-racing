059ed072638254b36a134b34427742ff12600e30
teraln <alen.terzic.fs@gmail.com>
2020-01-15 10:19:46 +0100

    Added basic components to Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,23 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <Navbar />
+    <TopParallax />
+    <PartnerBelt />
+    <v-container>
+      
+    </v-container>
+    <DriverCard />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import Navbar from "../components/Navbar";
+import TopParallax from "../components/TopParallax";
+import PartnerBelt from "../components/PartnerBelt";
+import DriverCard from "../components/Elements/DriverCard";
 
 export default {
   name: "home",
-  components: {}
+  components: { Navbar, TopParallax, PartnerBelt, DriverCard }
 };
 </script>
