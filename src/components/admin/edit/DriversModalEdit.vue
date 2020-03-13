10c185c87fc9542273d21b3fd942a22b33f284af
teraln <alen.terzic.fs@gmail.com>
2020-03-13 17:30:15 +0100

    Changed color of buttons in admin modals

--- src/components/admin/edit/DriversModalEdit.vue
+++ src/components/admin/edit/DriversModalEdit.vue
@@ -76,8 +76,8 @@
         </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn color="blue darken-1" text @click="dialog = false">Close</v-btn>
-          <v-btn color="blue darken-1" text @click="uploadImage(imageSnapshot);">Save</v-btn>
+          <v-btn color="primary" text @click="dialog = false">Close</v-btn>
+          <v-btn color="primary" text @click="uploadImage(imageSnapshot);">Save</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
