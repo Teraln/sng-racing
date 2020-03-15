1efdd65788c303650f342ab28866aa41829751ba
teraln <alen.terzic.fs@gmail.com>
2020-03-15 19:30:34 +0100

    Removed redundant html

--- src/views/admin/PartnersAdmin.vue
+++ src/views/admin/PartnersAdmin.vue
@@ -32,14 +32,6 @@
                     <PartnersModalEdit :partner="partner" :getPartners="getPartners" />
                     <v-btn text outlined color="accent" @click="deletePartner(partner.id)">delete</v-btn>
                   </v-card-actions>
-
-                  <v-expand-transition>
-                    <div v-show="show">
-                      <v-divider></v-divider>
-
-                      <v-card-text>I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.</v-card-text>
-                    </div>
-                  </v-expand-transition>
                 </v-card>
               </v-col>
             </v-row>
